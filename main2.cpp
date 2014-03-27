dabf9b9a637abdfb28956b44108534d073d8ff35
riady <riady_saz@yahoo.com>
2014-03-27 10:03:57 +0700

    cobain lagi

--- main2.cpp
+++ main2.cpp
@@ -4,13 +4,4 @@ using namespace std;
 
 int main(){
 	cout<<"Hello G!"<<endl;
-=======
-
-using namespace std;
-
-int main()
-{
-	cout << "Hi Git!" << endl
->>>>>>> 3087aea97d5b2bd59a04207208a6959b4305dc0e
-	return 0;
 }
